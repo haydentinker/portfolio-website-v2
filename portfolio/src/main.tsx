3024fc46d631844d766ff0d9355efca28a5f0bfc
Hayden Tinker <haydentinker613@gmail.com>
2026-03-16 11:05:18 -0400

    Adding analytics

--- portfolio/src/main.tsx
+++ portfolio/src/main.tsx
@@ -2,10 +2,11 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "@mantine/core/styles.css";
-
+import { Analytics } from "@vercel/analytics/next";
 import { MantineProvider } from "@mantine/core";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <Analytics />
     <MantineProvider defaultColorScheme="dark">
       <App />
     </MantineProvider>
